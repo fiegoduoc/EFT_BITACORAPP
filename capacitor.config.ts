e2a2cf36e0d8311c0dba96f7bd7fccc968e08840
Diego Fuentes Tapia <142534149+fiegoduoc@users.noreply.github.com>
2024-07-15 00:51:31 -0400

    Add files via upload
    eft - semana 9

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BITACORAPP',
+  webDir: 'www'
+};
+
+export default config;
